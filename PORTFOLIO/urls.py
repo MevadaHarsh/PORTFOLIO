107979d58f4c6aa6b8a35d809255fa145686187a
Harsh Mevada <harshmevada03@gmail.com>
2026-04-26 14:01:54 +0530

    fix homepage route

--- PORTFOLIO/urls.py
+++ PORTFOLIO/urls.py
@@ -18,8 +18,13 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("My Portfolio is Live 🚀")
 
 urlpatterns = [
+    path('', home),
     path('admin/', admin.site.urls),
     path('', include('harsh_mevada.urls')),
 ]
